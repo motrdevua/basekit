f769098c7517576a5554e63d5a1e12bc81261237
Igor Lukashev <48984371+motrdevua@users.noreply.github.com>
2021-02-11 22:32:49 +0200

    updated

--- gulpfile.js
+++ gulpfile.js
@@ -64,6 +64,9 @@ const webpackConfig = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
         },
       },
     ],
